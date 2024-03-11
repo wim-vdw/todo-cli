24859c9ff42ba744fc22f228d87a586e0b9e263a
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-03-11 18:28:21 +0100

    add feature to read data from file

--- cmd/show.go
+++ cmd/show.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/wim-vdw/todo-cli/internal/task"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -10,11 +12,21 @@ var showCmd = &cobra.Command{
 	Use:     "show",
 	Short:   "Display tasks from the To-Do list",
 	Aliases: []string{"display", "list"},
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("show called")
-	},
+	Run:     showTasks,
 }
 
 func init() {
 	rootCmd.AddCommand(showCmd)
+}
+
+func showTasks(cmd *cobra.Command, args []string) {
+	tasks, err := task.ReadTasks("./tasks.json")
+	if err != nil {
+		fmt.Println("Error reading datafile containing tasks.")
+		fmt.Println("Error message ->", err)
+		os.Exit(1)
+	}
+	for _, t := range tasks {
+		fmt.Println(t.Description)
+	}
 }
--- internal/task/task.go
+++ internal/task/task.go
@@ -1,5 +1,23 @@
 package task
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Task struct {
-	Description string
+	Description string `json:"description"`
+}
+
+func ReadTasks(filename string) ([]Task, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return []Task{}, err
+	}
+	var tasks []Task
+	err = json.Unmarshal(data, &tasks)
+	if err != nil {
+		return []Task{}, err
+	}
+	return tasks, nil
 }
