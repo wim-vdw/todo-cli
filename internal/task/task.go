e9e452a1c9dc369b7d31cbb7dbaca2228628b6bd
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-03-12 11:46:37 +0100

    fix error with negative taskIDs

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,9 +1,6 @@
 package task
 
 import (
-	"encoding/json"
-	"errors"
-	"os"
 	"strconv"
 )
 
@@ -12,8 +9,6 @@ type Task struct {
 	Priority    int    `json:"priority"`
 	position    int
 }
-
-type Tasks []Task
 
 func (t *Task) SetPriority(priority int) {
 	t.Priority = priority
@@ -32,41 +27,4 @@ func (t *Task) PrettyPriority() string {
 
 func (t *Task) PrettyPosition() string {
 	return "(" + strconv.Itoa(t.position) + ")"
-}
-
-func ReadTasks(filename string) (Tasks, error) {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return []Task{}, err
-	}
-	var tasks []Task
-	err = json.Unmarshal(data, &tasks)
-	if err != nil {
-		return []Task{}, err
-	}
-	for i, _ := range tasks {
-		tasks[i].position = i + 1
-	}
-	return tasks, nil
-}
-
-func SaveTasks(filename string, tasks Tasks) error {
-	data, err := json.Marshal(tasks)
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile(filename, data, 0644)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (t Tasks) DeleteTask(taskID int) (Tasks, error) {
-	if taskID > len(t) {
-		return []Task{}, errors.New("task-id does not exist")
-	}
-	taskID -= 1
-	t = append(t[:taskID], t[taskID+1:]...)
-	return t, nil
 }
---
+++ internal/task/tasks.go
@@ -0,0 +1,49 @@
+package task
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+type Tasks []Task
+
+func (t Tasks) DeleteTask(taskID int) (Tasks, error) {
+	if taskID <= 0 {
+		return []Task{}, errors.New("task-id can not be negative")
+	}
+	if taskID > len(t) {
+		return []Task{}, errors.New("task-id does not exist")
+	}
+	taskID -= 1
+	t = append(t[:taskID], t[taskID+1:]...)
+	return t, nil
+}
+
+func ReadTasks(filename string) (Tasks, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return []Task{}, err
+	}
+	var tasks []Task
+	err = json.Unmarshal(data, &tasks)
+	if err != nil {
+		return []Task{}, err
+	}
+	for i, _ := range tasks {
+		tasks[i].position = i + 1
+	}
+	return tasks, nil
+}
+
+func SaveTasks(filename string, tasks Tasks) error {
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
